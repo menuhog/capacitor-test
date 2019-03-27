5752690f466432fc9b8f56e7f6f2426f3c061fae
John Soo Hoo <jdsoohoo@u.northwestern.edu>
2019-03-27 16:55:51 +0000

    capacitor test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,21 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+    grabPhoto() {
+
+        const { Camera } = Plugins;
+
+        Camera.getPhoto({
+            quality: 100,
+            resultType: 'base64'
+        }).then((result) => {
+            console.log(result);
+        }).catch((err) => {
+            console.log(err);
+            console.log('No web implementation for plugin');
+        });
+
+    }
 
 }
